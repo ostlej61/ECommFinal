6611dd4dbfe83eae0ee2feb7660cae71231632c5
ostlej61 <ostlej61@uwosh.edu>
2020-12-18 07:50:50 -0600

    Added package.json

--- FinalWebServer.js
+++ FinalWebServer.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function(req, res) {
 	var q = url.parse(req.url, true);
@@ -24,9 +24,8 @@ http.createServer(function(req, res) {
 		return res.end();
 	});
 	
-}).listen(8080);
+}).listen(PORT);
 
-console.log("Server Listening on assigned Port 8080...");
-//console.log("Server Listening on assigned PORT...");
+console.log("Server Listening on assigned PORT...");
 
 //test
