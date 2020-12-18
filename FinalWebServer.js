f0a2f70a6d2c87cd059953c64f69a98b943151c5
ostlej61 <ostlej61@uwosh.edu>
2020-12-18 07:37:58 -0600

    Initial Commit

---
+++ FinalWebServer.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+//const PORT = process.env.PORT || 5000;
+
+http.createServer(function(req, res) {
+	var q = url.parse(req.url, true);
+	var filename = "." + q.pathname;
+
+	if (filename == './') {
+		filename = './index';
+	}
+
+	filename = filename + ".html";
+	console.log('loading:...' + filename);
+
+	fs.readFile(filename, function(err, data) {
+		if (err){
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			return res.end("404: Page Not Found");
+		}
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
+	
+}).listen(8080);
+
+console.log("Server Listening on assigned Port 8080...");
+//console.log("Server Listening on assigned PORT...");
+
+//test
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node FinalWebServer.js
